464995482a8cbf6163720555f1c6cf41c9c2688c
kevin <kevin1737@163.com>
2021-06-12 23:28:25 +0800

    Update: Neural Net Work Note

--- DeepLearningFoundation/Examples/Chapter3/Mnist.py
+++
@@ -1,2 +0,0 @@
-import numpy as np
-from dataset.mnist import load_mnist
---
+++ DeepLearningFoundation/Examples/Chapter3/MnistNeuralNetWork.py
@@ -0,0 +1,4 @@
+#--- Mnist classficate by Neural Net Work ---
+import numpy as np
+from dataset.mnist import load_mnist
+#import sys, os
