e874d85fc587093002cb188e110ed0bdcf9bd7eb
kevin <kevin1737@163.com>
2021-06-25 22:40:49 +0800

    Update: Chapter4 Notes

--- DeepLearningFoundation/Examples/Chapter4/trainNeuralnet.py
+++ DeepLearningFoundation/Examples/Chapter4/trainNeuralnet.py
@@ -4,7 +4,7 @@ sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
-from twoLayerNet import TwoLayerNet
+from twoLayersNet import TwoLayerNet
 
 # 读入数据
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
@@ -28,8 +28,8 @@ for i in range(iters_num):
     t_batch = t_train[batch_mask]
     
     # 计算梯度
-    #grad = network.numerical_gradient(x_batch, t_batch)
-    grad = network.gradient(x_batch, t_batch)
+    grad = network.numerical_gradient(x_batch, t_batch)
+    #grad = network.gradient(x_batch, t_batch)
     
     # 更新参数
     for key in ('W1', 'b1', 'W2', 'b2'):
