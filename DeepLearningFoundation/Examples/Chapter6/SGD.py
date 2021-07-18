4d04d37255a89639dc6d262c5ee524a09e8b0500
kevin <kevin1737@163.com>
2021-07-18 23:44:55 +0800

    Update: Learning Methods Notes

---
+++ DeepLearningFoundation/Examples/Chapter6/SGD.py
@@ -0,0 +1,10 @@
+class SGD:
+    
+    """随机梯度下降法（Stochastic Gradient Descent）"""
+
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key] 
